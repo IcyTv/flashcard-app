843e5cf129d0a1e80ea47d99e0af743d99e96899
Michael Finger <michael.finger@icytv.de>
2020-06-06 17:45:08 +0200

    Added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 import Loading from './components/Loading';
+
+Sentry.init({
+	dsn: 'https://4d92290421964df3bc443a8b8fba723b@o403798.ingest.sentry.io/5266816',
+	environment: process.env.NODE_ENV || 'production',
+	attachStacktrace: true,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
